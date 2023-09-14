62318274fee3bd2a82b5e41ea624862e8026175d
youngentry <gentry_@naver.com>
2023-09-14 19:40:31 +0900

    refactor: NavSide 컴포넌트 내부 구조 모듈화

---
+++ src/containers/NavSide/NavSideBody/NavSideBody.tsx
@@ -0,0 +1,31 @@
+import styles from "./NavSideBody.module.scss";
+
+const NavSideBody = () => {
+  const menus = [
+    { category: "기능 구현", subjects: ["페이지네이션", "레이지 로딩", "예약 기능"] },
+    { category: "시각화/알고리즘", subjects: ["bfs/dfs", "stack/queue", "brute force"] },
+    { category: "CS 지식", subjects: ["네트워크", "자료구조", "운영체제"] },
+  ];
+  return (
+    <>
+      {menus.map((menu) => {
+        return (
+          <ul key={menu.category} className={styles.category}>
+            <strong className={styles.strong}>{menu.category}</strong>
+            <ul>
+              {menu.subjects.map((subject) => {
+                return (
+                  <li key={subject} className={styles.subject}>
+                    <span>{subject}</span>
+                  </li>
+                );
+              })}
+            </ul>
+          </ul>
+        );
+      })}
+    </>
+  );
+};
+
+export default NavSideBody;
---
+++ src/containers/NavSide/NavSideHeader/NavSideHeader.tsx
@@ -0,0 +1,11 @@
+import styles from "./NavSideHeader.module.scss";
+
+const NavSideHeader = async () => {
+  return (
+    <header className={styles.container}>
+      <h2>블로그 프로필</h2>
+    </header>
+  );
+};
+
+export default NavSideHeader;
