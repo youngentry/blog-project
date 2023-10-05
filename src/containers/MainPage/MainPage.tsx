6a005b659db3efd4f9d274271b908fc7fd0e4526
youngentry <gentry_@naver.com>
2023-10-05 20:54:41 +0900

    style: 메인 페이지 좌측 소개 문구, 우측 슬라이더 적용 #24

--- src/containers/MainPage/MainPage.tsx
+++ src/containers/MainPage/MainPage.tsx
@@ -1,11 +1,25 @@
 import React from "react";
 import styles from "./MainPage.module.scss";
-import QuizSlider from "./QuizSlider/QuizSlider";
+import Slider from "./QuizSlider/QuizSlider";
 
 const MainPage = () => {
   return (
     <div className={styles.container}>
-      <QuizSlider />
+      <div className={styles.info}>
+        <h2 className={styles.mainTitle}>
+          {`Youngentry's`} <br /> Next13 + TypeScript
+          <br /> 블로그 프로젝트
+        </h2>
+        <p>
+          블로그 기능 어디까지 만들 수 있을까?
+          <br />
+          궁금하면 만들어 봐! (ง •_•)ง
+        </p>
+        <div className={styles.links}>Github Tistory</div>
+      </div>
+      <div className={styles.sliderContainer}>
+        <Slider />
+      </div>
     </div>
   );
 };
--- src/containers/MainPage/QuizSlider/QuizSlider.tsx
+++ src/containers/MainPage/QuizSlider/QuizSlider.tsx
@@ -1,7 +1,38 @@
-import React from "react";
+"use client";
 
-const QuizSlider = () => {
-  return <div></div>;
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import styles from "./QuizSlider.module.scss";
+import { Pagination, Autoplay } from "swiper/modules";
+
+const Slider = () => {
+  return (
+    <>
+      <Swiper
+        spaceBetween={30}
+        pagination={{
+          clickable: true,
+        }}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        modules={[Pagination, Autoplay]}
+        className={styles.swiper}
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
+    </>
+  );
 };
 
-export default QuizSlider;
+export default Slider;
