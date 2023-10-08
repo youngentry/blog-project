84471189e1a472799cf6576623c3ebed7138647f
youngentry <gentry_@naver.com>
2023-10-09 03:03:53 +0900

    feat: 좋아요 한 게시물 목록 html css #33

--- src/app/manage/category/page.tsx
+++ src/app/manage/category/page.tsx
@@ -1,4 +1,3 @@
-import Editor from "@/containers/Editor/PostEditor";
 import CategoryEdit from "@/containers/CategoryEdit/CategoryEdit";
 
 // 게시글 작성 페이지(Editor)로 라우트합니다.
---
+++ src/app/manage/likes/page.tsx
@@ -0,0 +1,12 @@
+import Likes from "@/containers/Likes/Likes";
+import React from "react";
+
+const ManageLikesRouter = () => {
+  return (
+    <div>
+      <Likes />
+    </div>
+  );
+};
+
+export default ManageLikesRouter;
