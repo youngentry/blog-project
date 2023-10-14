3d17b8bc78752fe9bf6a999c285730b2194b93c3
youngentry <gentry_@naver.com>
2023-10-14 22:31:47 +0900

    chore: move dependencies to dev only

--- src/containers/ManageLikes/components/ManageLikeItem/ManageLikeItem.tsx
+++ src/containers/ManageLikes/components/ManageLikeItem/ManageLikeItem.tsx
@@ -3,6 +3,7 @@
 import React from 'react';
 import Link from 'next/link';
 import { BsArrowUpRightSquare } from 'react-icons/bs';
+// eslint-disable-next-line
 import { sanitize } from 'dompurify';
 import Image from 'next/image';
 
