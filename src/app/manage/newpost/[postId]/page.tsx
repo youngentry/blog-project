c0f0e9c2495de01e85e2fd8228f6a4a3d4a629c4
youngentry <gentry_@naver.com>
2023-10-17 00:33:11 +0900

    refactor: params 타입 next Params로 변경+

--- src/app/manage/newpost/[postId]/page.tsx
+++ src/app/manage/newpost/[postId]/page.tsx
@@ -1,15 +1,17 @@
+import { Params } from 'next/dist/shared/lib/router/utils/route-matcher';
+
 import Editor from '@/containers/Editor/PostEditor';
 import { checkEditAuthor } from '@/utils/sessionCheck/checkEditAuthor';
 
 // 게시물 수정 페이지로 라우트합니다.
 // 라우트 전 수정 권한 검사를 server에서 합니다.
-const EditPostRouter = async ({ params }: any) => {
+const EditPostRouter = async ({ params }: Params) => {
   // 수정 권한 확인
   const canEdit: boolean = await checkEditAuthor(params.postId);
 
   return (
     <div>
-      <Editor postId={params.postId} canEdit={canEdit} />
+      <Editor canEdit={canEdit} />
     </div>
   );
 };
