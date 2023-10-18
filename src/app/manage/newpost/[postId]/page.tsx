50664bb3cb69e800c7a4a8a78c30525ce33eb7fc
youngentry <gentry_@naver.com>
2023-10-18 21:27:23 +0900

    style: footer css #36

--- src/app/manage/newpost/[postId]/page.tsx
+++ src/app/manage/newpost/[postId]/page.tsx
@@ -4,8 +4,6 @@ import { checkEditAuthor } from '@/utils/sessionCheck/checkEditAuthor';
 // 게시물 수정 페이지로 라우트합니다.
 // 라우트 전 수정 권한 검사를 server에서 합니다.
 const EditPostRouter = async ({ params }: any) => {
-  console.log(params);
-
   // 수정 권한 확인
   const canEdit: boolean = await checkEditAuthor(params.postId);
 
--- src/containers/Footer/Footer.tsx
+++ src/containers/Footer/Footer.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
 
 import styles from './Footer.module.scss';
+import { Logo } from '@/components/logos/logos';
+import { BsArrowDown } from 'react-icons/bs';
 
 // footer 레이아웃 컴포넌트입니다.
 const Footer = () => {
-  return <footer className={styles.container}>푸터</footer>;
+  return (
+    <footer className={styles.container}>
+      <h3>Project Info</h3>
+      <div className={styles.links}>
+        <Logo domainName='github' />
+        <Logo domainName='tistory' />
+      </div>
+      <div className={styles.madeBy}>
+        <span>Made by</span>
+        <a href='mailto:gentry_@naver.com'>gentry_@naver.com</a>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
