cd4a16acd0eea09a8779c379a1e952bc99009433
youngentry <gentry_@naver.com>
2023-11-02 21:21:55 +0900

    chore: env variable 수정

--- src/utils/sessionCheck/checkBlogAdmin.ts
+++ src/utils/sessionCheck/checkBlogAdmin.ts
@@ -4,7 +4,7 @@
  * @returns boolean
  */
 export const checkBlogAdmin = (email: string): boolean => {
-  const admins: string[] | undefined = process.env.NEXT_PUBLIC_BLOG_ADMIN?.split(','); // blog manager emails: string[]
+  const admins: string[] | undefined = process.env.BLOG_ADMIN?.split(','); // blog manager emails: string[]
 
   // 로그인된 유저의 email이 admin에 포함될 경우 true를 반환합니다.
   if (email && admins) {
