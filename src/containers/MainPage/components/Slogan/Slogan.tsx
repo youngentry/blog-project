3449892e2460d902c89b609b3f1fc3effb9025f0
youngentry <gentry_@naver.com>
2023-11-02 18:22:07 +0900

    feat: 랜딩페이지 간격, 문구 수정

--- src/containers/MainPage/components/Slogan/Slogan.tsx
+++ src/containers/MainPage/components/Slogan/Slogan.tsx
@@ -8,11 +8,6 @@ const Slogan = () => {
         {`Youngentry's`} <br /> Next13 + TypeScript
         <br /> 블로그 프로젝트
       </h2>
-      <p className={styles.description}>
-        블로그 기능 어디까지 만들 수 있을까?
-        <br />
-        궁금하면 만들어 봐! (ง •_•)ง
-      </p>
       <div className={styles.links}>
         <Logo domainName='github' />
         <Logo domainName='tistory' />
